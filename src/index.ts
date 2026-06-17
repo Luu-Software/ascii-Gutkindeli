d8f2b0dfeec3ddacbdff66acfc1dcc31bfba7a77
Gutkindeli <gutkindeli@gmail.com>
2026-06-17 16:07:26 -0300

    lapele

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
-  // COMPLETAR
+  consoleLog(letra * cantidad)
 }
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 
-// COMPLETAR
+while (tamaño > 0){
+  console.log(caracter)
+ tamaño= tamaño -1
+}
+
