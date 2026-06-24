a19a357175fe56689108cb54efb6607f215fd676
Gutkindeli <gutkindeli@gmail.com>
2026-06-23 21:25:05 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import { preguntar } from './lib/consola.ts';
 
+
 function repetirLetra(letra: string, cantidad: number): string {
-  consoleLog(letra * cantidad)
+  let resultado : string= "";
+
+
+  let vecesRepetido: number = 0
+ 
+  while (vecesRepetido < cantidad){
+    resultado= resultado + letra
+    vecesRepetido = vecesRepetido + 1
+  }
+  return resultado;
 }
+
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
+let resultado: string = repetirLetra (caracter, tamaño);
+let vecesRepetido: number = 1;
 
-while (tamaño > 0){
-  console.log(caracter)
- tamaño= tamaño -1
+
+while (vecesRepetido < tamaño) {
+  console.log(resultado)
+  vecesRepetido = vecesRepetido + 1;
 }
+  
 
